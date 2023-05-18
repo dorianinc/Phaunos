03893ea018a1b502ef00c79914d39d9726d8b36f
dorianinc <dorianrdba@gmail.com>
2023-05-18 09:13:28 -0700

    completed all crud operations for bookmarks

--- app/api/bookmarks_routes.py
+++ app/api/bookmarks_routes.py
@@ -4,11 +4,57 @@ from app.models import db, Bookmark, Bookmarks_List
 
 bookmarks_routes = Blueprint("bookmarks", __name__)
 
+@bookmarks_routes.route("/<int:bookmark_id>")
+def get_bookmark_by_id(bookmark_id):
+    """" Get single bookmark by id """
+    user = current_user.to_dict()
+    
+    bookmark = Bookmark.query.get(bookmark_id)
+    #------------ validation -------------#    
+    if not bookmark:
+        error = make_response("Bookmark does not exist")
+        error.status_code = 404
+        return error
+    
+    list = Bookmarks_List.query.get(bookmark.bookmarks_list_id)
+    if list.user_id != user["id"]:
+        error = make_response("Only the creator can view their Bookmark")
+        error.status_code = 401
+        return error
+    #--------------------------------------#  
+    
+    return bookmark.to_dict()
+
+@bookmarks_routes.route("/<int:bookmark_id>", methods=["PUT"])
+def edit_review(bookmark_id):
+    """ Edit a bookmark """
+    user = current_user.to_dict()
+    data = request.get_json()
+
+    bookmark = Bookmark.query.get(bookmark_id)
+    #------------ validation -------------#    
+    if not bookmark:
+        error = make_response("Bookmark does not exist")
+        error.status_code = 404
+        return error
+    
+    list = Bookmarks_List.query.get(bookmark.bookmarks_list_id)
+    if list.user_id != user["id"]:
+        error = make_response("Only the creator of a Bookmark can delete a bookmark")
+        error.status_code = 401
+        return error 
+    #--------------------------------------#  
+    
+    bookmark.completed = data["completed"]
+    db.session.commit()
+        
+    return bookmark.to_dict()
+
+
 @bookmarks_routes.route("/<int:bookmark_id>", methods=["DELETE"])
 def delete_bookmark(bookmark_id):
     """ Delete a single bookmark """
     user = current_user.to_dict()
-    print(f"user 👉 {user}")
 
     #------------ validation -------------#    
     bookmark = Bookmark.query.get(bookmark_id)
