7f77145dff28e99d3895b58f021fff24aad86c3a
dorianinc <dorianrdba@gmail.com>
2023-05-22 18:59:00 -0700

    updated logo

--- react-app/src/components/Navigation/LogoButton/index.js
+++ react-app/src/components/Navigation/LogoButton/index.js
@@ -1,11 +1,10 @@
-import logo from "./logos/logo-green2.png"
 import "./LogoButton.css"
 
 function LogoButton() {
   
     return (
       <div className="logoContainer">
-          <img className ="logo" alt="phaunos"  src={logo}/>
+          <img className ="logo" alt="phaunos"  src="/images/logo.png"/>
           <p className="title">Phaunos</p>
       </div>
     );
