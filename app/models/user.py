426d87e7fe5d2c42f67d2da2fce7e886caa67b8f
dorianinc <dorianrdba@gmail.com>
2023-06-19 09:50:46 -0700

    commented not inactive table

--- app/models/user.py
+++ app/models/user.py
@@ -2,8 +2,6 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-
-
 follows = db.Table(
     "follows",
     db.Column("follower", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True),
